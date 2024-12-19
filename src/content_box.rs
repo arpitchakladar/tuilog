687a3873cff6a0e2bc10b7b162933670de2a8b83
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2024-12-20 00:58:33 +0530

    Centered the input fields.

--- src/content_box.rs
+++ src/content_box.rs
@@ -9,9 +9,15 @@ use cursive::views::{
 	Button,
 	PaddedView,
 };
-use cursive::traits::*;
+use cursive::view::{
+	Nameable,
+	View,
+	Resizable,
+};
+use cursive::theme::Theme;
 use cursive::Cursive;
 
+use crate::utils::longest_line;
 use crate::theme::{
 	get_edit_view_theme,
 	get_hostname_art_theme,
@@ -19,6 +25,20 @@ use crate::theme::{
 use crate::message::draw_error_message;
 
 const INPUT_LENGTH: usize = 24;
+
+fn draw_input_field<T: View>(label: &str, theme: Theme, left_spacing: usize, edit_view: T) -> PaddedView<LinearLayout> {
+	PaddedView::lrtb(left_spacing, 0, 0, 0,
+		LinearLayout::horizontal()
+			.child(TextView::new(format!("{}: [", label)))
+			.child(
+				ThemedView::new(
+					theme,
+					edit_view
+				)
+			)
+			.child(TextView::new("]"))
+	)
+}
 
 pub fn draw_content_box(siv: &mut Cursive) {
 	let edit_view_theme = get_edit_view_theme(&siv);
@@ -28,6 +48,10 @@ pub fn draw_content_box(siv: &mut Cursive) {
 		Ok(art) => art,
 		Err(_) => hostname,
 	};
+
+	let hostname_art_width = longest_line(&hostname_art);
+	// The padded space to the left of each input fields to center it
+	let input_left_space = (hostname_art_width - INPUT_LENGTH - 12) / 2;
 
 	siv.add_layer(
 		Dialog::around(
@@ -41,10 +65,11 @@ pub fn draw_content_box(siv: &mut Cursive) {
 							),
 						)
 					)
-					.child(LinearLayout::horizontal()
-						.child(TextView::new("Username: "))
-						.child(ThemedView::new(
+					.child(
+						draw_input_field(
+							"Username",
 							edit_view_theme.clone(),
+							input_left_space,
 							EditView::new()
 								.filler(" ")
 								.on_submit(|siv, _| {
@@ -52,22 +77,22 @@ pub fn draw_content_box(siv: &mut Cursive) {
 								})
 								.with_name("username")
 								.fixed_width(INPUT_LENGTH)
-							)
-						)
+						),
 					)
-					.child(LinearLayout::horizontal()
-						.child(TextView::new("Password: "))
-						.child(ThemedView::new(
-							edit_view_theme,
+					.child(
+						draw_input_field(
+							"Password",
+							edit_view_theme.clone(),
+							input_left_space,
 							EditView::new()
 								.secret()
 								.filler(" ")
 								.on_submit(|siv, _| {
-									draw_error_message(siv, "Couldn't log you in.");
+									draw_error_message(siv, "Could not log you in!");
 								})
 								.with_name("password")
-								.fixed_width(INPUT_LENGTH)
-						))
+								.fixed_width(INPUT_LENGTH),
+						)
 					)
 					.child(
 						PaddedView::lrtb(0, 0, 1, 0,
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,9 @@
-pub fn longest_line(input: &str) -> Option<&str> {
-	input.lines().max_by_key(|line| line.len())
+use unicode_width::UnicodeWidthStr;
+
+pub fn longest_line(input: &str) -> usize {
+	input
+		.lines()
+		.map(|line| UnicodeWidthStr::width(line))
+		.max()
+		.unwrap_or(0)
 }
