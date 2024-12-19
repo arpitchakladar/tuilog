525b15bbe2f3f37b308a8ec0b8b5117154489d1a
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2024-12-19 22:29:04 +0530

    Added error logo ascii icon to the error dialog.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ use cursive::views::{
 	Button,
 	PaddedView,
 	Canvas,
-	Layer
+	Layer,
+	ResizedView,
 };
 use cursive::traits::*;
 use cursive::theme::{
@@ -17,12 +18,14 @@ use cursive::theme::{
 	Theme,
 	ColorType,
 	Color,
-	ColorStyle
+	ColorStyle,
+	BaseColor
 };
+use cursive::align::HAlign;
 use cursive::{
 	Cursive,
 	Printer,
-	CursiveExt
+	CursiveExt,
 };
 
 const INPUT_LENGTH: usize = 24;
@@ -36,45 +39,71 @@ fn get_edit_view_theme(siv: &Cursive) -> Theme {
 	edit_view_theme
 }
 
-fn draw_background(siv: &mut Cursive) {
-	let ascii_art = fs::read_to_string("assets/ascii-art.txt")
-		.expect("Failed to read ASCII art file. Ensure the file exists and is readable.");
+fn draw_background_ascii_art(siv: &mut Cursive) {
+	match fs::read_to_string("assets/ascii-art.txt") {
+		Ok(ascii_art) => {
+			let background_color = siv.current_theme().palette[PaletteColor::Background];
+			// Create a Canvas to render ASCII art
+			let ascii_view = Canvas::new(ascii_art.to_string())
+				.with_draw(move |ascii_art, printer: &Printer| {
+					let lines: Vec<&str> = ascii_art.lines().collect();
+					let art_height = lines.len();
+					let art_width = lines.iter().map(|line| line.chars().count()).max().unwrap_or(0);
 
-	let background_color = siv.current_theme().palette[PaletteColor::Background];
-	// Create a Canvas to render ASCII art
-	let ascii_view = Canvas::new(ascii_art.to_string())
-		.with_draw(move |ascii_art, printer: &Printer| {
-			let lines: Vec<&str> = ascii_art.lines().collect();
-			let art_height = lines.len();
-			let art_width = lines.iter().map(|line| line.chars().count()).max().unwrap_or(0);
+					let screen_height = printer.size.y;
+					let screen_width = printer.size.x;
 
-			let screen_height = printer.size.y;
-			let screen_width = printer.size.x;
+					// Calculate centered position
+					let start_y = (screen_height.saturating_sub(art_height)) / 2;
+					let start_x = (screen_width.saturating_sub(art_width)) / 2;
 
-			// Calculate centered position
-			let start_y = (screen_height.saturating_sub(art_height)) / 2;
-			let start_x = (screen_width.saturating_sub(art_width)) / 2;
+					for (i, line) in lines.iter().enumerate() {
+						printer.with_color(ColorStyle::new(
+							ColorType::Color(Color::parse("#2bbac5").unwrap()),
+							ColorType::Color(background_color)
+						), |printer| {
+							printer.print((start_x, start_y + i), line);
+						})
+					}
+				});
 
-			for (i, line) in lines.iter().enumerate() {
-				printer.with_color(ColorStyle::new(
-					ColorType::Color(Color::parse("#2bbac5").unwrap()),
-					ColorType::Color(background_color)
-				), |printer| {
-					printer.print((start_x, start_y + i), line);
-				})
-			}
-		});
-
-	// Add the ASCII art as the background
-	siv.add_layer(Layer::new(ascii_view.full_screen()));
+			// Add the ASCII art as the background
+			siv.add_layer(Layer::new(ascii_view.full_screen()));
+		},
+		Err(_) => println!("Failed to read ASCII art file. Ensure the file exists and is readable.")
+	}
 }
 
-fn draw_dialog(siv: &mut Cursive, text: &str) {
+fn get_error_theme(siv: &Cursive) -> Theme {
+	let mut error_theme = siv.current_theme().clone();
+	error_theme.palette[PaletteColor::Primary] = Color::Dark(BaseColor::Red);
+	error_theme
+}
+
+fn draw_error_dialog(siv: &mut Cursive, text: &str) {
+	let ascii_art = fs::read_to_string("assets/logos/error.txt")
+		.expect("Failed to read ASCII art file");
+	let error_theme = get_error_theme(siv);
 	siv.add_layer(
 		Dialog::around(
-			PaddedView::lrtb(2, 2, 1, 1,
+			PaddedView::lrtb(0, 0, 1, 1,
 				LinearLayout::vertical()
-					.child(TextView::new(text))
+					.child(
+						ThemedView::new(
+							error_theme,
+							PaddedView::lrtb(0, 0, 0, 1,
+								TextView::new(ascii_art)
+									.h_align(HAlign::Center)
+							)
+						)
+					)
+					.child(
+						ResizedView::with_fixed_size(
+							(50, 1),
+							TextView::new(text)
+								.h_align(HAlign::Center)
+						)
+					)
 					.child(
 						PaddedView::lrtb(0, 0, 1, 0,
 							Button::new_raw("[OK]", |siv| { siv.pop_layer(); })
@@ -82,7 +111,6 @@ fn draw_dialog(siv: &mut Cursive, text: &str) {
 					)
 			)
 		)
-		.title("ERROR")
 	);
 }
 
@@ -115,7 +143,7 @@ fn draw_content_box(siv: &mut Cursive) {
 								.secret()
 								.filler(" ")
 								.on_submit(|siv, _| {
-									draw_dialog(siv, "Couldn't log you in login.");
+									draw_error_dialog(siv, "Couldn't log you in.");
 								})
 								.with_name("password")
 								.fixed_width(INPUT_LENGTH)
@@ -136,7 +164,7 @@ fn main() {
 	let mut siv = Cursive::new();
 	siv.load_toml(include_str!("../assets/theme.toml")).unwrap();
 
-	draw_background(&mut siv);
+	draw_background_ascii_art(&mut siv);
 	draw_content_box(&mut siv);
 
 	siv.run();
