90f601b1c8e937c4345186afd8c423ca8c39dc89
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2024-12-19 20:12:49 +0530

    Added padding to the container/Dialog.
    Removed the <> from the button and added [] instead.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::fs;
-use std::io::{self, Read};
-use std::sync::{Arc, Mutex};
+use cursive::views::{
+	Dialog,
+	EditView,
+	LinearLayout,
+	TextView,
+	ThemedView,
+	Button,
+	PaddedView
+};
 use cursive::traits::*;
-use cursive::views::{Dialog, EditView, LinearLayout, TextView, ThemedView};
-use cursive::theme::{BaseColor, Color, Palette, PaletteColor, Theme};
-use cursive::align::HAlign;
+use cursive::theme::{PaletteColor, Theme};
 use cursive::{Cursive, CursiveExt};
 
 fn get_edit_view_theme(siv: &Cursive) -> Theme {
@@ -16,7 +20,7 @@ fn get_edit_view_theme(siv: &Cursive) -> Theme {
 	edit_view_theme.palette[PaletteColor::Highlight] = edit_view_theme.palette[PaletteColor::Background];
 	edit_view_theme.palette[PaletteColor::HighlightText] = edit_view_theme.palette[PaletteColor::Background];
 	edit_view_theme.palette[PaletteColor::HighlightInactive] = edit_view_theme.palette[PaletteColor::Background];
-	return edit_view_theme;
+	edit_view_theme
 }
 
 fn main() {
@@ -24,38 +28,40 @@ fn main() {
 	siv.load_toml(include_str!("../assets/theme.toml")).unwrap();
 
 	let edit_view_theme = get_edit_view_theme(&siv);
+
 	// Creates a dialog with a single "Quit" button
 	siv.add_layer(
 			Dialog::around(
-				LinearLayout::vertical()
-					.child(LinearLayout::horizontal()
-						.child(TextView::new("Username: "))
-						.child(ThemedView::new(
-							edit_view_theme.clone(),
-							EditView::new()
-								.filler(" ")
-								.with_name("username")
-								.fixed_width(20)
+				PaddedView::lrtb(2, 2, 1, 1,
+					LinearLayout::vertical()
+						.child(LinearLayout::horizontal()
+							.child(TextView::new("Username: "))
+							.child(ThemedView::new(
+								edit_view_theme.clone(),
+								EditView::new()
+									.filler(" ")
+									.with_name("username")
+									.fixed_width(20)
+								)
 							)
 						)
+						.child(LinearLayout::horizontal()
+							.child(TextView::new("Password: "))
+							.child(ThemedView::new(
+								edit_view_theme,
+								EditView::new()
+									.secret()
+									.filler(" ")
+									.with_name("password")
+									.fixed_width(20)
+							))
+						)
+						.child(Button::new_raw("[LOGIN]", |s| s.quit())
 					)
-					.child(LinearLayout::horizontal()
-						.child(TextView::new("Password: "))
-						.child(ThemedView::new(
-							edit_view_theme,
-							EditView::new()
-								.secret()
-								.filler(" ")
-								.with_name("password")
-								.fixed_width(20)
-						))
-					)
+				)
 			)
 			.title("TuiLog")
-			.button("Submit", |s| s.quit())
-			.h_align(HAlign::Center)
 		);
 
-	// Starts the event loop.
 	siv.run();
 }
